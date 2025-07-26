06bab223df2a0b0239bfc21761962bed24f4f90a
Mashruf Rohon <mashrufrohon@gmail.com>
2025-07-26 21:01:16 +0600

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require('cypress');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = defineConfig({
+  video: false,
+  screenshotOnRunFailure: true,
+  e2e: {
+    baseUrl: 'https://www.saucedemo.com/',
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
+  },
+  env: {
+    allure: true,
+  }
+});
